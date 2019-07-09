c1d80191b9580a6385db5f8c997e8a95ed865a50
felix_gm <88125469@qq.com>
2019-07-09 17:11:58 +0800

    .

---
+++ Example/Pods/Target Support Files/QGBasicCode/QGBasicCode-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_QGBasicCode : NSObject
+@end
+@implementation PodsDummy_QGBasicCode
+@end
---
+++ Example/QGBasicCode/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  QGBasicCode
+//
+//  Created by felix_gm on 07/09/2019.
+//  Copyright (c) 2019 felix_gm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
